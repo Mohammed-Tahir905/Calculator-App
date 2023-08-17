3992dd2f4a7580cd14456b76facac1f92312c978
Mohammed Tahir <tahirb541@gmail.com>
2023-08-17 21:43:29 +0500

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/chap8_calculator/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chap8_calculator
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constant.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+Color orange=Color(0xffffa00a);
+Color grey=Colors.grey;
+Color white=Colors.white;
